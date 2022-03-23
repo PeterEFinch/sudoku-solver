0faccad0559e76ce925ff7f19a943ad177f2e0e1
pfinch <peter.finch@graphmasters.net>
2022-03-23 19:42:01 +1000

    :bulb: Improving documentation

--- cli/server/main.go
+++ cli/server/main.go
@@ -18,21 +18,16 @@ func main() {
 	address := flag.String("address", ":8080", "the address for the server")
 	flag.Parse()
 
-	// Adds handlers to mux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", handlers.Health)
 	mux.HandleFunc("/solve", handlers.Solve)
-
-	// Creates server
 	server := &http.Server{
 		Addr:    *address,
 		Handler: mux,
 	}
 
-	// Allows the server to gracefully shutdown
 	shutdownOnSignal(server, os.Interrupt)
 
-	// Starts server
 	log.Info().Str("address", *address).Msg("starting server")
 	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
 		log.Error().Err(err).Str("address", *address).Msg("listen and serve failed")
@@ -40,32 +35,30 @@ func main() {
 	}
 }
 
-// shutdownOnSignal will shutdown the server when one of the
-// given signals is sent. It is not blocking.
+// shutdownOnSignal will attempt to gracefully shutdown the server when one of
+// the given signals is sent.
+//
+// The waiting for a signal is done in a go routine to ensure that this method
+// is not blocking.
 func shutdownOnSignal(server *http.Server, signals ...os.Signal) {
 	if len(signals) == 0 {
 		log.Warn().Msg("server is not listening to any shutdown signals")
 		return
 	}
 
-	// Creates channel for signals to be notified on
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, signals...)
 
 	go func() {
-		// Waits for signal
 		sig := <-signalCh
 		signal.Stop(signalCh)
 		close(signalCh)
 		log.Info().Str("signal", sig.String()).Msg("server shutting down")
 
-		// Gracefully shuts down the server
 		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 		if err := server.Shutdown(ctx); err != nil {
 			log.Warn().Err(err).Msg("failed to shutdown server")
 		}
-
-		// Cancel context
 		cancel()
 	}()
 }
